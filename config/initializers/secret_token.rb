e70efe36c8a75ad64206bcda3d836f400108f189
Thibault Poisson <thibault.poisson@gmail.com>
2010-11-23 15:19:03 +0100

    commit initial

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Parkmycar::Application.config.secret_token = '0fb06460a6685bea80822fc2c27920ca4110db187f7b63bc41921d3c067e504b7251bcb813147a3c6f91dfacb053962d08d209e49c9f7942a35265f97f905abe'

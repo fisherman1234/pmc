c8aa35dc2e0398e9bafa36a75a7e9b81120e5a51
Thibault Poisson <thibault.poisson@gmail.com>
2010-11-23 17:42:36 +0100

    tables crées

--- db/migrate/20101123163457_create_users.rb
+++ db/migrate/20101123163457_create_users.rb
@@ -2,7 +2,7 @@ class CreateUsers < ActiveRecord::Migration
   def self.up
     create_table :users do |t|
       t.string :first_name
-      t.strinf :last_name
+      t.string :last_name
       t.string :civility
       t.string :email
       t.float :account_balance
